809797e2081f4aa3f7ab1dc439dc48fa3a96d476
fryklund <fryklund.fredrik@gmail.com>
2023-05-11 11:28:06 -0400

    update test suite by removing PUX tests

--- tests/test_matvec.jl
+++ tests/test_matvec.jl
@@ -10,13 +10,14 @@ using LinearAlgebra
     panelorder = 16
     curve = AnalyticDomains.starfish(n_arms = 3, amplitude=0.2)
     dcurve = CurveDiscretization.discretize(curve, numpanels, panelorder)
+    S = zeros(Float64,0)
     N = dcurve.numpoints
     # Compute
     x = rand(N)
 		flhs = system_matvec(dcurve)
     LHS = LinearMaps.LinearMap(flhs, dcurve.numpoints)
 		yfmm = LHS(x)
-    A = system_matrix(dcurve)
+    A = system_matrix(dcurve,S)
     ydir = A*x
     erel = norm(yfmm-ydir, Inf) / norm(ydir, Inf)
     @test erel < 5e-14
